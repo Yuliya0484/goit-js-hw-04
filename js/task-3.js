21d31b424c65a3d30cb866def14323c68efadbb2
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2024-09-14 21:41:53 +0200

    firsttask

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,9 @@
 "use strict";
-function filterArray(numbers, value) {
-  const filterNumbers = [];
+const profile = {
+  username: "Jacob",
+  playTime: 300,
 
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > value) {
-      filterNumbers.push(numbers[i]);
-    }
-  }
-  return filterNumbers;
-}
-
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+  getInfo() {},
+  changeUserName() {},
+  updatePlayTime() {},
+};
